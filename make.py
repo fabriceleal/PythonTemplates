228ecd0a52ae52d995e73534e755605b5dcc0ba8
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-03-24 14:52:40 -0700

    final fixes, output and template dirs given by command line

--- make.py
+++ make.py
@@ -8,31 +8,35 @@ import sys
 try:
 
 	d = {}
-	outdirectory = './out'
-
+	outdirectory    = './out'
+	template_folder = './template'
+	
 	# Parse command line args here
 	args = sys.argv[1:]
-	for a in args:
-		print a
-	print len(args)
+
 	try:
-		if len(args) < 4:
-			raise Exception('Invalid number of arguments (%d), expected at least 4' % (len(args)))
+		if len(args) != 6:
+			raise Exception('Invalid number of arguments (%d), expected 6' % (len(args)))
 		# Build d from args
-		d['name'] = args[0]
-		d['version'] = args[1]
-		d['author'] = args[2]
+		d['name']        = args[0]
+		d['version']     = args[1]
+		d['author']      = args[2]
 		d['description'] = args[3]
 
-		if len(args) > 4:
-			outdirectory = args[4]
+		if args[4] != None and args[4] != '':
+			outdirectory    = args[4]
+		if args[5] != None and args[5] != '':
+			template_folder = args[5]		
+
 	except: 
 	        print "Exception parsing command line args."
-		print "Usage: %s <module_name> <module_version> <module_author> <module_description> [out_dir]" % (sys.argv[0])
+		print "Usage: %s <module_name> <module_version> <module_author> <module_description> <out_dir> <description>" % (sys.argv[0])
         	raise
 
 	def walk_callback(arg, directory, files):
 		for f in files:
+			inp = None
+			out = None
 			# Parse file 
 			try:
 				print "Processing %s at directory %s" % (f, directory)
@@ -48,16 +52,14 @@ try:
 					# Perform transformations
 					t_name    = t_name.substitute(d)
 					t_content = t_content.substitute(d)
-
-					
-					# Check if dir exists; if not, create
-					
+				
 					# Generate full path for the output file
 					full_t_name = os.path.normpath(os.path.join(outdirectory, t_name))
 					
 					# Ugly, horrendous and non-portable way of taking of the filename of a filename's full path
 					full_dir_name = '/'.join(full_t_name.split('/')[:-1])
 
+					# Create the dir if it doesn't exists
 					if not os.path.exists(full_dir_name):
 						os.makedirs(full_dir_name, 0777) # Hardcoded chmod ... no problem, right?
 				
@@ -69,14 +71,15 @@ try:
 			except Exception, e:
 				print "Exception file %s at directory %s" % (f, directory)
 				raise
-			finally:
-				pass
-				#inp.close()
-				#out.close()
+			finally:								
+				if inp != None:
+					inp.close()
+				if out != None:
+					out.close()
 		# for
 	# def walk_callback
 
-	os.path.walk('./template', walk_callback, None)
+	os.path.walk(template_folder, walk_callback, None)
 	
 except Exception, e:
 	pprint(sys.exc_info())
